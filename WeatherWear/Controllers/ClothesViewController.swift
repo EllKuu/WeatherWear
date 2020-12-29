f5471bad63fbbec31927af42a9a71af98679016e
elliott kung <elliottkung@elliotts-MacBook-Pro.local>
2020-12-29 15:27:11 -0500

    initial commit

---
+++ WeatherWear/Controllers/ClothesViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ClothesViewController.swift
+//  WeatherWear
+//
+//  Created by elliott kung on 2020-12-29.
+//
+
+import UIKit
+
+class ClothesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+       
+    }
+
+}
---
+++ WeatherWear/Controllers/WeatherViewController.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherViewController.swift
+//  WeatherWear
+//
+//  Created by elliott kung on 2020-12-29.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+    }
+
+}
